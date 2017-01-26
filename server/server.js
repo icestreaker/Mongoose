d79f6ace567ad5d1a8ea01d87a6557e952a62f5b
James Warren Basham <James Warren Basham>
2017-01-25 16:06:17 -0800

    last commit

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -40,6 +41,7 @@ app.get('todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo})
   }).catch((e) => {
     res.status(400).send();
@@ -55,8 +57,8 @@ app.get('todos/:id', (req, res) => {
 // }).catch((e) => console.log(e));
 
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
